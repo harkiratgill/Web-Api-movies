f3f99e955bcbfe9dfed305e77716282973f03f93
Harkirat Singh Gill <Sunnysinghgill78@gmail.com>
2019-05-08 14:36:20 +1200

    main

---
+++ MVC Video/Controllers/VideoController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Video.Models ;
+
+namespace MVC_Video.Controllers
+{
+    public class VideoController : Controller
+    {
+        // GET: Video
+        public ActionResult Index()
+        {
+            using (mvcEntities1 dbModel = new mvcEntities1())
+            {
+                return View(dbModel.Movies.ToList());//this list all the movies the are in the database
+            }
+                
+        }
+
+        // GET: Video/Details/5
+        public ActionResult Details(int id)
+        {
+            using (mvcEntities1 dbModel = new mvcEntities1())
+            {
+                return View(dbModel.Movies.Where(x=>x.Id== id).FirstOrDefault());//this selects the id of the movie and show it details
+            }
+                
+        }
+
+        // GET: Video/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Video/Create
+        [HttpPost]
+        public ActionResult Create(Movy movie)
+        {
+            try
+            {
+                using (mvcEntities1 dbModel = new mvcEntities1())//this add the movie to the databse
+                {
+                    dbModel.Movies.Add(movie);
+                    dbModel.SaveChanges();
+                }
+
+                    // TODO: Add insert logic here
+
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Video/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (mvcEntities1 dbModel = new mvcEntities1())
+            {
+                return View(dbModel.Movies.Where(x => x.Id == id).FirstOrDefault());//this selects the id of the movie
+            }
+        }
+
+        // POST: Video/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Movy movies)
+        {
+            try
+            {
+                using (mvcEntities1 dbModel = new mvcEntities1())
+                {
+                    dbModel.Entry(movies).State = EntityState.Modified;//this edits the movie 
+                    dbModel.SaveChanges();
+                }
+                return RedirectToAction("Index");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Video/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (mvcEntities1 dbModel = new mvcEntities1())
+            {
+                return View(dbModel.Movies.Where(x => x.Id == id).FirstOrDefault());//this selets the movie to be deleted 
+            }
+        }
+
+        // POST: Video/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (mvcEntities1 dbModel = new mvcEntities1())
+                {
+                    Movy movies = dbModel.Movies.Where(x => x.Id == id).FirstOrDefault();//this delets the movie 
+                    dbModel.Movies.Remove(movies);
+                    dbModel.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC Video/index.aspx
@@ -0,0 +1,55 @@
+﻿
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Movie Info</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">/ <!--these are the external css-->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+        <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <!--this is the nav bar-->
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span> 
+                    <span class="icon-bar"></span>
+                    <a href="Video/Index">Back Home</a>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    
+                     <a href="Video/Index">Back Home</a> <!--this takes you back home-->
+                   
+
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="jumbotron">
+            <h3 class="text-center">Search For Any Movie</h3><!--this is the heading-->
+            <form id="searchForm">
+                <input type="text" class="form-control" id="searchText" placeholder="Search Movie...."> <!--this is the serch bar-->
+            </form>
+        </div>
+    </div>
+    <div class="container">
+        <div id="movies" class="row"></div>
+    </div>
+
+    
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script> <!--these are the scripts-->
+    <script src="js/axios.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+
+</html>
